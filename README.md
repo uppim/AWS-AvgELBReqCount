78a379adb351b4e6d2d6b75b36232c5a6b701942
upendra <mupendra47@gmail.com>
2016-02-23 19:23:30 +0530

    Initial script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AWS-AvgELBReqCount
-Cloudwatch custom metric for average requests for ELB.
+Cloudwatch custom metric for average requests for ELB.This script has to run as a cron job  to push custom metric to cloudwatch.
+This is useful for autoscaling instances based on the number of requests ELB is receiving and number of healthy hosts.
+
+Pre-Requisite 
+##############
+Aws Cli installed and configured.
+
+
---
+++ scripts/averagerequestcount.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
+starttime=`date -u +"%Y-%m-%dT%H:%M:%S"`
+endtime=`date -u +"%Y-%m-%dT%H:%M:%S" --date '+1 minute'`
+echo "st : $starttime"
+echo "et : $endtime"
+
+healthycount=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name HealthyHostCount --dimensions Name=LoadBalancerName,Value=grabpress-uat --statistics Average --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Average'/ {print +$2}'`
+echo "hc : $healthycount"
+
+totalrequests=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name RequestCount --dimensions Name=LoadBalancerName,Value=grabpress-uat --statistics Sum --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Sum'/ {print +$2}'`
+echo "tr : $totalrequests"
+
+avgRequests=$(($totalrequests/$healthycount))
+
+echo "AR : $avgRequests"
+aws cloudwatch put-metric-data --metric-name avgReq --namespace "Pramati/AvgReq" --value $avgRequests
