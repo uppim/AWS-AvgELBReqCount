f617397afebef623aa20a9aa9289876601311d40
upendra <mupendra47@gmail.com>
2016-02-23 21:23:11 +0530

    Initial script

--- scripts/averagerequestcount.sh
+++ scripts/averagerequestcount.sh
@@ -7,10 +7,10 @@ endtime=`date -u +"%Y-%m-%dT%H:%M:%S" --date '+1 minute'`
 echo "st : $starttime"
 echo "et : $endtime"
 
-healthycount=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name HealthyHostCount --dimensions Name=LoadBalancerName,Value=grabpress-uat --statistics Average --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Average'/ {print +$2}'`
+healthycount=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name HealthyHostCount --dimensions Name=LoadBalancerName,Value=abc --statistics Average --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Average'/ {print +$2}'`
 echo "hc : $healthycount"
 
-totalrequests=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name RequestCount --dimensions Name=LoadBalancerName,Value=grabpress-uat --statistics Sum --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Sum'/ {print +$2}'`
+totalrequests=`aws cloudwatch get-metric-statistics --namespace "AWS/ELB" --metric-name RequestCount --dimensions Name=LoadBalancerName,Value=abc --statistics Sum --start-time $starttime --end-time $endtime --period 60 --region us-east-1 | awk ' /'Sum'/ {print +$2}'`
 echo "tr : $totalrequests"
 
 avgRequests=$(($totalrequests/$healthycount))
